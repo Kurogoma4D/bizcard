d032233c4a6da5dfcf75d9819164bd245edf0842
Kurogoma4D <highdro0479@gmail.com>
2019-02-23 10:34:30 +0900

    fixed TestPlane class

--- js/testobj.js
+++ js/testobj.js
@@ -9,14 +9,14 @@ class TestPlane {
             color: 0xffffff
         });
         var geometry = new THREE.PlaneGeometry(1, 1);
-        obj = new THREE.Mesh(geometry, material);
-        obj.overdraw = true;
-        obj.name = name;
-        obj.position.set(x, y, z);
-        obj.rotation.set(-Math.PI / 2.0, 0, 0);
+        this.obj = new THREE.Mesh(geometry, material);
+        this.obj.overdraw = true;
+        this.obj.name = name;
+        this.obj.position.set(x, y, z);
+        this.obj.rotation.set(-Math.PI / 2.0, 0, 0);
     }
 
     getObj(){
-        return obj;
+        return this.obj;
     }
 }
